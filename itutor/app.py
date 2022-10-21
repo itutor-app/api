a723e6725b777189128ae829b0c5172d761feeca
Elielson68 <elielsonbr.com@gmail.com>
2022-10-20 22:41:39 -0300

    alterando dados para IClound

--- itutor/app.py
+++ itutor/app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from itutor.extensions.configure import init_app, load_extensions
-
+from extensions.configure import init_app, load_extensions
+import socket
 app = Flask(__name__)
 print("Criou o app flask")
 init_app(app)
 load_extensions(app)
 
 if __name__ == "__main__":
-  print("Rodou o app")
-  app.run(host="0.0.0.0", port=443)
+  print("Rodou o app, ip", socket.gethostbyname(socket.gethostname()))
+  app.run(host="0.0.0.0", port=8080, debug=True)
